57457379fcef81114e12cbf67fecdec60984d119
Milo Sones <102179233+MiloSones@users.noreply.github.com>
2025-02-08 23:46:29 +0000

    Add getWords to frontend

--- frontend/src/components/fileUpload.jsx
+++ frontend/src/components/fileUpload.jsx
@@ -13,10 +13,27 @@ export default function FileUpload() {
       const customTrackers = ["ws://thomascarey.co.uk:44851"];
 
       client.seed(files, { announce: customTrackers }, (torrent) => {
-        console.log("Client is seeding " + torrent.magnetURI);
+        getWords(torrent.magnetURI);
       });
     }
   }
 
   return <input multiple type="file" onChange={onFileChange} />;
+}
+
+
+async function getWords(magnetURI) {
+  try {
+    const response = await fetch("http://localhost:4000/generateWords", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Specify JSON content type
+      },
+      body: JSON.stringify({ inputString: magnetURI }), // Convert to JSON
+    });
+
+    console.log(await response.json());
+  } catch (e) {
+    console.error(e);
+  }
 }
